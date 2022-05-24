b4d1c229efec2afb8e3d6c1e5f388e310808486f
Tim Morris <timthewebguy@gmail.com>
2022-05-24 09:25:06 -0700

    refactor(types): simplify range type

--- packages/types/src/bounds.ts
+++ packages/types/src/bounds.ts
@@ -1,10 +1,26 @@
-export interface Bounds {
-  minX: number
-  maxX: number
-  minY: number
-  maxY: number
+export interface Range {
+  min: number
+  max: number
 }
 
-export interface BoundedValue<T> extends Bounds {
+export interface ValuedRange<T> extends Range {
+  value: T
+}
+
+export interface DirectedRange {
+  start: number
+  end: number
+}
+
+export interface ValuedDirectedRange<T> extends DirectedRange {
+  value: T
+}
+
+export interface Bounds {
+  x: Range
+  y: Range
+}
+
+export interface ValuedBounds<T> extends Bounds {
   value: T
 }
--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,4 +1,11 @@
-export type { Bounds, BoundedValue } from './bounds'
+export type {
+  Bounds,
+  DirectedRange,
+  Range,
+  ValuedBounds,
+  ValuedRange,
+  ValuedDirectedRange,
+} from './bounds'
 export type { Matrix4x4, Matrix4x3 } from './matrix'
 export type { Point, PointObject, PointTuple } from './points'
 export type { Monotonicity, Spline, SplineMetadata, Axis, LUTEntry } from './spline'
