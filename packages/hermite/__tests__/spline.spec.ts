f3abca19e2c6fca21db8c995a8db76eb0f496b36
Tim Morris <timthewebguy@gmail.com>
2022-06-24 13:19:02 -0400

    chore(hermite): improve tests

--- packages/hermite/__tests__/spline.spec.ts
+++ packages/hermite/__tests__/spline.spec.ts
@@ -1,38 +1,8 @@
 import { describe, test } from 'vitest'
-import { PointObject } from '@curvy/types'
-// import { toPath } from '@curvy/visualize'
-import { createCubicHermiteSpline, createCubicTCBSpline } from '../src'
-
-describe('createCubicHermiteSpline', () => {
-  test.skip('creates a valid cubic bezier', ({ expect }) => {
-    const points: [PointObject, PointObject, PointObject, PointObject] = [
-      { x: 0, y: 100 },
-      { x: 50, y: 0 },
-      { x: 125, y: 0 },
-      { x: 100, y: 0 },
-    ]
-
-    const precision = 5
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const spline = createCubicHermiteSpline(points, { precision, lutResolution: 512 })
-
-    // console.log(toPath(spline, 200))
-
-    // for (const t of [0, 0.1, 0.25, 0.5, 0.75, 0.9, 1]) {
-    //   const solvedPoint = solveCubicDeCasteljau(points, t)
-    //
-    //   expect(spline.solveY(solvedPoint.x)).toBe(roundTo(solvedPoint.y, precision))
-    //   expect(spline.solveX(solvedPoint.y)).toBe(roundTo(solvedPoint.x, precision))
-    // }
-
-    expect(2).toBe(2)
-  })
-})
+import { createCubicTCBSpline } from '../src'
 
 describe('createCubicTCBSpline', () => {
   test('it creates a tcb spline', ({ expect }) => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const spline = createCubicTCBSpline(
       [
         [0, 0],
@@ -44,8 +14,12 @@ describe('createCubicTCBSpline', () => {
       { tension: -1, continuity: 0, bias: -0, virtualStart: [-50, 0], virtualEnd: [150, 50] }
     )
 
-    // console.log(toPath(spline, 200))
+    expect(spline.solveY(0)).toBe(0)
+    expect(spline.solveY(25)).toBe(50)
+    expect(spline.solveY(50)).toBe(25)
+    expect(spline.solveY(75)).toBe(100)
+    expect(spline.solveY(100)).toBe(50)
 
-    expect(2).toBe(2)
+    // expect(2).toBe(2)
   })
 })
