5117e4cc68fdf14fe28f26dd0c97477a80c1e822
Tim Morris <timthewebguy@gmail.com>
2024-03-04 10:03:50 -0500

    feat: split by segments

--- packages/core/src/splines.ts
+++ packages/core/src/splines.ts
@@ -1,4 +1,34 @@
-import type { Matrix4x4 } from './types'
+import invariant from 'tiny-invariant'
+import type { CubicScalars, Matrix4x4 } from './types'
+
+export function toCubicScalars(
+  values: Array<number>,
+  stride: number,
+): Array<CubicScalars> {
+  const chunks: Array<CubicScalars> = []
+
+  invariant(values.length >= 4, 'Values must have at least 4 elements')
+  invariant(
+    stride === 1 || stride === 2 || stride === 3,
+    'Stride must be 1, 2, or 3',
+  )
+  if (stride === 3) {
+    invariant(
+      values.length % stride === 1,
+      'Values length must be a multiple of 3 plus 1',
+    )
+  } else {
+    invariant(
+      values.length % stride === 0,
+      `Values length must be a multiple of ${stride}`,
+    )
+  }
+
+  for (let i = 0; i <= values.length - 4; i += stride) {
+    chunks.push([values[i]!, values[i + 1]!, values[i + 2]!, values[i + 3]!])
+  }
+  return chunks
+}
 
 export const bezier: Matrix4x4 = [
   [-1, 3, -3, 1],
@@ -7,12 +37,20 @@ export const bezier: Matrix4x4 = [
   [1, 0, 0, 0],
 ]
 
+export function toBezierSegments(values: Array<number>) {
+  return toCubicScalars(values, 3)
+}
+
 export const hermite: Matrix4x4 = [
   [2, 1, -2, 1],
   [-3, -2, 3, -1],
   [0, 1, 0, 0],
   [1, 0, 0, 0],
 ]
+
+export function toHermiteSegments(values: Array<number>) {
+  return toCubicScalars(values, 2)
+}
 
 export const cardinal = (a: number): Matrix4x4 => [
   [-a, 2 - a, a - 2, a],
@@ -21,12 +59,20 @@ export const cardinal = (a: number): Matrix4x4 => [
   [0, 1, 0, 0],
 ]
 
+export function toCardinalSegments(values: Array<number>) {
+  return toCubicScalars(values, 1)
+}
+
 export const catmullRom: Matrix4x4 = [
   [-0.5, 1.5, -1.5, 0.5],
   [1, -2.5, 2, -0.5],
   [-0.5, 0, 0.5, 0],
   [0, 1, 0, 0],
 ]
+
+export function toCatmullRomSegments(values: Array<number>) {
+  return toCubicScalars(values, 1)
+}
 
 const ONE_SIXTH = 1 / 6
 export const bSpline: Matrix4x4 = [
@@ -35,3 +81,7 @@ export const bSpline: Matrix4x4 = [
   [-0.5, 0, 0.5, 0],
   [ONE_SIXTH, 4 * ONE_SIXTH, ONE_SIXTH, 0],
 ]
+
+export function toBSplineSegments(values: Array<number>) {
+  return toCubicScalars(values, 1)
+}
