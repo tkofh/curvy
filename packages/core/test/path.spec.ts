1979b7fa970d107ccc3e055938bd8d3f92e1fade
Tim Morris <timthewebguy@gmail.com>
2024-12-03 19:24:20 -0500

    quadratic path 2d

--- packages/core/test/path.spec.ts
+++ packages/core/test/path.spec.ts
@@ -1,7 +1,10 @@
 import { describe, expect, test } from 'vitest'
 import * as linearCurve2d from '../src/curve/linear2d'
+import * as quadraticCurve2d from '../src/curve/quadratic2d'
 import * as linearPath2d from '../src/path/linear2d'
+import * as quadraticPath2d from '../src/path/quadratic2d'
 import * as linearPolynomial from '../src/polynomial/linear'
+import * as quadraticPolynomial from '../src/polynomial/quadratic'
 import { round } from '../src/util'
 import * as vector2 from '../src/vector/vector2'
 
@@ -102,5 +105,88 @@ describe('linear2d', () => {
         ),
       ),
     ).toBe(round(1 + Math.sqrt(2)))
+  })
+})
+describe('quadratic2d', () => {
+  test('fromCurves', () => {
+    expect(
+      quadraticPath2d.fromCurves(
+        quadraticCurve2d.fromPolynomials(
+          quadraticPolynomial.make(0, 0, 1),
+          quadraticPolynomial.make(0, 0, 1),
+        ),
+        quadraticCurve2d.fromPolynomials(
+          quadraticPolynomial.make(0, 0, 1),
+          quadraticPolynomial.make(0, 0, 1),
+        ),
+      ),
+    ).toBeDefined()
+  })
+  test('fromCurveArray', () => {
+    expect(
+      quadraticPath2d.fromCurveArray([
+        quadraticCurve2d.fromPolynomials(
+          quadraticPolynomial.make(0, 0, 1),
+          quadraticPolynomial.make(0, 0, 1),
+        ),
+        quadraticCurve2d.fromPolynomials(
+          quadraticPolynomial.make(0, 0, 1),
+          quadraticPolynomial.make(0, 0, 1),
+        ),
+      ]),
+    ).toBeDefined()
+  })
+  test('isQuadratic2dPath', () => {
+    expect(
+      quadraticPath2d.isQuadraticPath2d(
+        quadraticPath2d.fromCurves(
+          quadraticCurve2d.fromPolynomials(
+            quadraticPolynomial.make(0, 0, 1),
+            quadraticPolynomial.make(0, 0, 1),
+          ),
+          quadraticCurve2d.fromPolynomials(
+            quadraticPolynomial.make(0, 0, 1),
+            quadraticPolynomial.make(0, 0, 1),
+          ),
+        ),
+      ),
+    ).toBe(true)
+  })
+  test('append', () => {
+    const c0 = quadraticCurve2d.fromPolynomials(
+      quadraticPolynomial.make(0, 0, 1),
+      quadraticPolynomial.make(0, 0, 1),
+    )
+    const c1 = quadraticCurve2d.fromPolynomials(
+      quadraticPolynomial.make(0, 0, 1),
+      quadraticPolynomial.make(0, 0, 1),
+    )
+    const p0 = quadraticPath2d.fromCurves(c0)
+
+    const p1Curves = [...p0]
+
+    expect(p1Curves).toEqual([c0])
+
+    const p1 = quadraticPath2d.append(p0, c1)
+
+    const p2Curves = [...p1]
+
+    expect(p2Curves).toEqual([c0, c1])
+  })
+  test('solve', () => {
+    const p = quadraticPath2d.fromCurves(
+      quadraticCurve2d.fromPolynomials(
+        quadraticPolynomial.make(0, 0, 1),
+        quadraticPolynomial.make(0, 0, 1),
+      ),
+      quadraticCurve2d.fromPolynomials(
+        quadraticPolynomial.make(1, 0, 1),
+        quadraticPolynomial.make(1, 0, 1),
+      ),
+    )
+
+    expect(quadraticPath2d.solve(p, 0)).toEqual(vector2.make(0, 0))
+    expect(quadraticPath2d.solve(p, 0.5)).toEqual(vector2.make(1, 1))
+    expect(quadraticPath2d.solve(p, 1)).toEqual(vector2.make(2, 2))
   })
 })
